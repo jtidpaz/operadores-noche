b4510d15c85776690d2df1ded194d374ee8b419d
jtidpaz <juanca1@hotmail.es>
2024-09-26 20:49:58 -0500

    se crea el archivo OperadoresDeCompracion.py

---
+++ operadoresProject/OperadoresDeComparacion.py
@@ -0,0 +1,11 @@
+#Ejemplo de Operadores de Comparacion
+
+m= int (input("ingrese el valor de M:"))
+n = int (input("ingrese el valor de N:"))
+
+print(m == n) #Igualdad
+print(m > n) # mayor que >
+print(m < n) # menor que <
+print(m >= n) #mayor o igual que >=
+print(m <= n) #menor o igual que <=
+print(m != n) # No igual o diferente !
